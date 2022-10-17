9981508dad7fab92699a4c2476b68a728d25dae1
SpeekeR <DominikZappe1999@seznam.cz>
2022-10-17 09:40:28 +0200

    GLFW hello world window

--- main.cpp
+++ main.cpp
@@ -1,61 +1,37 @@
 #include <glfw/glfw3.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-static void error_callback(int error, const char* description) {
-    fputs(description, stderr);
-}
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
-
-int main() {
+int main(int argc, char **argv)
+{
     GLFWwindow* window;
 
-    glfwSetErrorCallback(error_callback);
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
-    if(!glfwInit())
-        exit(EXIT_FAILURE);
-
-    window = glfwCreateWindow(640, 480, "Simple Example", NULL, NULL);
-    if(!window) {
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
         glfwTerminate();
-        exit(EXIT_FAILURE);
+        return -1;
     }
 
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
 
-    glfwSetKeyCallback(window, key_callback);
-
-    while(!glfwWindowShouldClose(window)) {
-        float ratio;
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
-        glViewport(0, 0, width, height);
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
+
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
+        /* Poll for and process events */
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
     glfwTerminate();
-    exit(EXIT_SUCCESS);
+    return 0;
 }
